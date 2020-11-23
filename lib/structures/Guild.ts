74916b78539206a08cac358b20530e5e930a6b5e
bloatoo <bloatoo@arch>
2020-11-23 15:24:25 +0200

    banner image

--- lib/structures/Guild.ts
+++ lib/structures/Guild.ts
@@ -2,6 +2,7 @@ import Channel from './Channel.ts';
 import User from './User.ts';
 import { GuildType } from "./Interfaces.ts";
 import Role from "./Role.ts";
+import { Member } from '../../mod.ts';
 
 export default class Guild {
     private _id: string;              	                                            //guild id
@@ -34,7 +35,7 @@ export default class Guild {
     private _unavailable?:	boolean;                                                //true if this guild is unavailable due to an outage
     private _memberCount?: number;	                                                //total number of members in this guild
     private _voiceStates?: object[]; /*partial voice state object array (todo?)*/	//states of members currently in voice channels; lacks the guild_id key
-    private _members?: User[]; /*TODO: Member Object*/                              //users in the guild
+    private _members?: Member[]; /*TODO: Member Object*/                              //users in the guild
     private _channels?: Channel[];	                                                //channels in the guild
     private _presences?: object[];	/*TODO: partial presence update object*/	    //presences of the members in the guild, will only include non-offline members if the size is greater than large threshold
     private _maxPresences?: number;	                                                //the maximum number of presences for the guild (the default value, currently 25000, is in effect when null is returned)
@@ -49,10 +50,7 @@ export default class Guild {
     private _maxVideoChannelUsers?:	number;	                                        //the maximum amount of users in a video channel
     private _approximateMemberCount?: number;	                                    //approximate number of members in this guild, returned from the GET /guild/<id> endpoint when with_counts is true
     private _approximatePresenceCount?: number;	                                    //approximate number of non-offline members in this guild, returned from the GET /guild/<id> endpoint when with_counts is true
-    
-    private _coreData: GuildType;
     constructor(data: GuildType) {
-        this._coreData = data;
         /*======================================================
         ONLY IMPLEMENTING NECESSARY PROPERTIES, DO NOT REFORMAT.
         ======================================================*/
